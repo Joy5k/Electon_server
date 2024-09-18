8396f9d6a69c252f14e649ec89f0ed05da89e9f5
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-09-19 00:55:24 +0600

    Created the auth controller

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import catchAsync from "../../../shared/catchAsync";
-import { AuthServices } from "./auth.service";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import { AuthServices } from "./auth.services";
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
     const result = await AuthServices.loginUser(req.body);
@@ -24,63 +24,63 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
     })
 });
 
-const refreshToken = catchAsync(async (req: Request, res: Response) => {
-    const { refreshToken } = req.cookies;
+// const refreshToken = catchAsync(async (req: Request, res: Response) => {
+//     const { refreshToken } = req.cookies;
 
-    const result = await AuthServices.refreshToken(refreshToken);
+//     const result = await AuthServices.refreshToken(refreshToken);
 
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Access token generated successfully!",
-        data: result
+//     sendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: "Access token generated successfully!",
+//         data: result
      
-    })
-});
+//     })
+// });
 
-const changePassword = catchAsync(async (req: Request, res: Response) => {
-    const user = req.user;
-    const result = await AuthServices.changePassword(user, req.body);
+// const changePassword = catchAsync(async (req: Request, res: Response) => {
+//     const user = req.user;
+//     const result = await AuthServices.changePassword(user, req.body);
 
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Password Changed successfully",
-        data: result
-    })
-});
+//     sendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: "Password Changed successfully",
+//         data: result
+//     })
+// });
 
-const forgotPassword = catchAsync(async (req: Request, res: Response) => {
+// const forgotPassword = catchAsync(async (req: Request, res: Response) => {
 
-    await AuthServices.forgotPassword(req.body);
+//     await AuthServices.forgotPassword(req.body);
 
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Check your email!",
-        data: null
-    })
-});
+//     sendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: "Check your email!",
+//         data: null
+//     })
+// });
 
-const resetPassword = catchAsync(async (req: Request, res: Response) => {
+// const resetPassword = catchAsync(async (req: Request, res: Response) => {
 
-    const token = req.headers.authorization || "";
+//     const token = req.headers.authorization || "";
 
-    await AuthServices.resetPassword(token, req.body);
+//     await AuthServices.resetPassword(token, req.body);
 
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Password Reset!",
-        data: null
-    })
-});
+//     sendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: "Password Reset!",
+//         data: null
+//     })
+// });
 
 
 export const AuthController = {
     loginUser,
-    refreshToken,
-    changePassword,
-    forgotPassword,
-    resetPassword
+    // refreshToken,
+    // changePassword,
+    // forgotPassword,
+    // resetPassword
 };
