d46d0765de1f9530f2920e1b536de4f0154ba727
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-09-19 00:45:48 +0600

    created the created json web token reusable function

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,7 +1,38 @@
-import express from "express";
+import express from 'express';
+import { AuthController } from './auth.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
-router.get('/all-users',)
+router.post(
+    '/login',
+    AuthController.loginUser
+);
 
-export const userRoutes=router;
+router.post(
+    '/refresh-token',
+    AuthController.refreshToken
+)
+
+router.post(
+    '/change-password',
+    auth(
+        UserRole.SUPER_ADMIN,
+        UserRole.ADMIN,
+        UserRole.USER
+    ),
+    AuthController.changePassword
+);
+
+router.post(
+    '/forgot-password',
+    AuthController.forgotPassword
+);
+
+router.post(
+    '/reset-password',
+    AuthController.resetPassword
+)
+
+export const AuthRoutes = router;
