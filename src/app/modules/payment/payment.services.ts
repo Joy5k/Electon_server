74fd60491f7e89f4f50975a9b30bac63b10e4e7e
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-11-29 10:00:00 +0600

    creating the product qunatity handler after successfully purcahse

--- src/app/modules/payment/payment.services.ts
+++ src/app/modules/payment/payment.services.ts
@@ -1,12 +1,20 @@
+import httpStatus from "http-status";
 import config from "../../config";
-
-
+import CustomError from "../../error/customError";
+import mongoose from "mongoose";
 const stripe = require('stripe')(config.stripe_secret)
+
+
 const createPaymentIntent=async(payload:any,Token:any):Promise<void>=>{
 
     const price = payload.amount;
     const amountInCents = Math.round(price * 100);
+try {
+    const session=await mongoose.startSession()
 
+} catch (error) {
+    throw new CustomError(httpStatus.SERVICE_UNAVAILABLE,"something went wrong ")
+}
     const paymentIntent = await stripe.paymentIntents.create({
         currency: 'usd',
         amount: amountInCents,
