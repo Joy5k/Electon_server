33da8a0a5ea35c8647e7094321beb551832a004f
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-12-01 10:58:54 +0600

    Created the sells history service

---
+++ src/app/modules/sellsHistory/sellsHistory.services.ts
@@ -0,0 +1,11 @@
+import catchAsync from "../../../shared/catchAsync";
+
+
+const createSellHistoryIntoDB=catchAsync(async(req,res)=>{
+
+})
+
+export const sellsHistoryServices={
+
+    createSellHistoryIntoDB,
+}
