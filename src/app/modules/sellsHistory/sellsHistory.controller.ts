b646c1ecebb34f00c484efcf1f789b2a6846d363
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-12-01 11:00:01 +0600

    created the sell history controller

---
+++ src/app/modules/sellsHistory/sellsHistory.controller.ts
@@ -0,0 +1,11 @@
+import catchAsync from "../../../shared/catchAsync";
+
+
+const createSellHistory=catchAsync(async(req,res)=>{
+
+})
+
+export const sellsHistoryController={
+
+    createSellHistory,
+}
--- src/app/modules/sellsHistory/sellsHistory.services.ts
+++ src/app/modules/sellsHistory/sellsHistory.services.ts
@@ -1,9 +1,9 @@
 import catchAsync from "../../../shared/catchAsync";
 
 
-const createSellHistoryIntoDB=catchAsync(async(req,res)=>{
+const createSellHistoryIntoDB=async(req,res)=>{
 
-})
+}
 
 export const sellsHistoryServices={
 
