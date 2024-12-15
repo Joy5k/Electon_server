76dd9652010183404969026cd017c6cd77faeda2
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-12-15 23:27:17 +0600

    complete the chating model

--- src/app/modules/chat/chat.model.ts
+++ src/app/modules/chat/chat.model.ts
@@ -5,5 +5,15 @@ const createChattingSchema=new Schema<IChat> ({
     room:{
         type:String,
         required:[true,"Room unable to create! may have doesn't room email"]
-    }
+    },
+    text:{
+        type:String,
+        required:[true,"message is required"],
+        default:"N/A"
+    },
+    sender:{
+        type:String,
+        required:[true,"sender email is not available"]
+    },
+
 })
